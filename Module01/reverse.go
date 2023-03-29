e59497b905aa75594b649dc263461970976531ae
Manu-Abuya <manuabuya69@gmail.com>
2023-03-30 00:53:46 +0300

    reverse test

--- Module01/reverse.go
+++ Module01/reverse.go
@@ -3,6 +3,8 @@ package module01
 // This will return the provided word in reverse order
 func Reverse(word string) string {
 	var res string
-
+	for i := len(word) - 1; i >= 0; i-- {
+		res = res + string(word[i])
+	}
 	return res
 }
